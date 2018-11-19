aed88e3481218d4ce470f5bfab09f1a7a0dfed20
Randy Piper <tbcat23@gmail.com>
2018-11-19 12:33:39 -0500

    Add a placeholder API key

--- src/api/mbta.js
+++ src/api/mbta.js
@@ -1,11 +1,12 @@
-const URL = 'http://api-v3.mbta.com'
-
+const URL = 'http://api-v3.mbta.com';
+const KEY = ''; // TODO provide an API key
+    
 async function getRoutes(stop) {
-	return fetch(URL + '/routes?filter[stop]=' + stop);
+	return fetch(URL + '/routes?filter[stop]=' + stop + '&api_key=' + KEY);
 }
 
 async function getPredictions(stop) {
-	return fetch(URL + '/predictions?filter[stop]=' + stop);
+	return fetch(URL + '/predictions?filter[stop]=' + stop + '&api_key=' + KEY);
 }
 
 export { getRoutes, getPredictions };
